24256cdc659dd4ecbf9b44e10b1c8c6aa9352834
Nay <43801463+NayRojas@users.noreply.github.com>
2018-11-18 23:34:12 -0500

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,20 @@
 window.cipher = {
-  // ... 
+  // ...
 };
+
+
+---------------------------------------------
+function cifrar(){
+//Acá empiezo a almacenar en variables los inputs del programa: mensaje, offset, cifrar
+	str = prompt('tu mensaje');
+  	num = prompt('tu offset');
+  let result = "";
+
+    for (let i = 0; i < str.length; i++){
+      let numAscii = str.charCodeAt(i);
+      if (numAscii >= 65 && numAscii <= 90){
+        result += String.fromCharCode((numAscii - 65 +num) % 26 + 65);
+      }
+    }
+return result;
+}
