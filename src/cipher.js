0fded601af39cbbef7c6ff61cdb892881eb2c3b8
Nay <nayllen.rojas@gmail.com>
2018-11-21 13:00:54 -0500

    Separación de funciones cifrar/descifrar y eventos del DOM / aceptación de espacio

--- src/cipher.js
+++ src/cipher.js
@@ -4,60 +4,36 @@
 	decode:
 };*/
 
-//Caputaramos los inputs del DOM
-let userMessage = document.getElementById('message');
-let userKey = document.getElementById('offset');
-let cifrarButton = document.getElementById('cifrar-button');
-let descifrarButton = document.getElementById('descifrar-button');
-let borrarButton = document.getElementById('erase');
-
-//**********************EVENTO DEL BOTON CIFRAR**************************
-cifrarButton.addEventListener('click', () => {
-	const message = userMessage.value;
-	const key = userKey.value;
-	cifrar(message, key)
-});
-
 //****************************FUNCION CIFRAR*******************************
 const cifrar = (message, key) => {
-let result = "";
-let numAscii;
 
 //RECORRIDO DEL FOR
 for (let i = 0; i < message.length; i++){
 		numAscii = message.charCodeAt(i);
 
-		if (document.getElementById("message").value == "") {
-			document.getElementById('error-campo-vacio').innerHTML = "¡Ingresa tu mensaje!";
-			console.log("entro");
-		}else{
 //Asimila mayusculas
 			if (numAscii >= 65 && numAscii <= 90){
 				numAscii = String.fromCharCode((numAscii - 65 + parseInt(key)) % 26 + 65);
 				result += numAscii;
+				/*let userMessage = document.getElementById('message').value;
+				let replace = userMessage.replace( userMessage, result);
+				document.getElementById("message").value = replace;*/
 				console.log(result);
 //Asimila minisculas
 			} else if (numAscii >= 97 && numAscii <= 122){
 				numAscii = String.fromCharCode((numAscii - 97 + parseInt(key)) % 26 + 97);
 				result += numAscii;
-				let userMessage = document.getElementById('message').value;
+				/*let userMessage = document.getElementById('message').value;
 				let replace = userMessage.replace( userMessage, result);
-				document.getElementById("message").value = replace;
+				document.getElementById("message").value = replace;*/
 				console.log(result);
 //Asimila el espacio " "
 			} else if (numAscii === 32){
 				result += " ";
 			}
-		}
 	}
 };
 
-//**********************EVENTO DEL BOTON DESCIFRAR**************************
-descifrarButton.addEventListener('click', () => {
-	const message = userMessage.value;
-	const key = userKey.value;
-	descifrar(message, key)
-});
 
 //**************************FUNCION DESCIFRAR*******************************
 
@@ -67,41 +43,26 @@ let result = "";
 //**RECORRIDO DEL FOR
 		for (let i = 0; i < message.length; i++){
 			let numAscii = message.charCodeAt(i);
-			console.log(numAscii[i]);
+			let userMessage = document.getElementById('message').value;
+			let replace = userMessage.replace( userMessage, result);
+			document.getElementById("message").value = replace;
+			console.log(result);
 
 //Asimila mayusculas
 			if (numAscii >= 65 && numAscii <= 90){
 				numAscii = String.fromCharCode((numAscii + 65 - parseInt(key)) % 26 + 65);
 				result += numAscii;
-				console.log("result" + result);
-				document.getElementById("message").innerHTML = result;
+				console.log(result);
+
 //Asimila minisculas
 			} else if (numAscii >= 97 && numAscii <= 122){
 				numAscii = String.fromCharCode((numAscii + 97 - parseInt(key)) % 26 + 97);
 				result += numAscii;
-				let userMessage = document.getElementById('message').value;
-				let replace = userMessage.replace( userMessage, result);
-				document.getElementById("message").value = replace;
 				console.log(result);
+
 //Asimila el espacio " "
 			} else if (numAscii === 32){
 				result += " ";
 			}
 		}
 	}
-
-
-//*******************************FUNCION BORRAR*******************************
-
-		borrarButton.addEventListener('click', () => {
-			const message = userMessage.value;
-			const key = userKey.value;
-
-			borrar()
-			console.log("entro");
-		});
-
-		const borrar = () => {
-			 document.getElementById("message").value = "";
-			 document.getElementById("offset").value = "";
-		};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
-//Caputaramos los inputs y variables del DOM
+let userMessage = document.getElementById('message');
+let userKey = document.getElementById('offset');
+let cifrarButton = document.getElementById('cifrar-button');
+let descifrarButton = document.getElementById('descifrar-button');
+let borrarButton = document.getElementById('erase');
+let result = "";
+let numAscii;
 
-//*****************EVENTO PARA BOTON CIFRAR*****************************
-//incluir alertas de inputs
+//**********************EVENTO DEL BOTON CIFRAR**************************
+cifrarButton.addEventListener('click', () => {
+	let message = userMessage.value;
+	let key = userKey.value;
+  cifrar(message, key)
+
+
+  let userMessageCifrado = document.getElementById('message').value;
+  let replace = userMessageCifrado.replace( userMessageCifrado, result);
+  document.getElementById("message").value = replace;
+});
+
 
 //*****************EVENTO PARA BOTON DESCIFRAR*****************************
 //incluir alertas de inputs
 
-//*****************EVENTO PARA BOTON BORRAR*****************************
+descifrarButton.addEventListener('click', () => {
+	let message = userMessage.value;
+	let key = userKey.value;
+  descifrar(message, key)
+
+  let userMessageDescifrado = document.getElementById('message').value;
+  console.log("entro")
+  let replace = userMessageDescifrado.replace( userMessageDescifrado, result);
+  document.getElementById("message").value = replace;
+});
+
+//*******************************FUNCION BORRAR*******************************
+
+borrarButton.addEventListener('click', () => {
+	const message = userMessage.value;
+	const key = userKey.value;
+
+	borrar()
+	console.log("entro");
+});
+
+const borrar = () => {
+	 document.getElementById("message").value = "";
+	 document.getElementById("offset").value = "";
+};
+
+//*******************************FUNCION COPIAR TEXTO*******************************
