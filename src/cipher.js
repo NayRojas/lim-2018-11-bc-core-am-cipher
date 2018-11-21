be570a2dcf6e53c7d6046ee140d30e7d18b5803a
nayrojas <nayllen.rojas@gmail.com>
2018-11-20 22:12:21 -0500

    solucionando el problema del  spacio en la funcion de cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -22,27 +22,27 @@ cifrarButton.addEventListener('click', () => {
 
 const cifrar = (message, key) => {
 let result = "";
+let numAscii;
 
-//**RECORRIDO DEL FOR
-		for (let i = 0; i < message.length; i++){
-			let numAscii = message.charCodeAt(i);
+//RECORRIDO DEL FOR
+	for (let i = 0; i < message.length; i++){
+			numAscii = message.charCodeAt(i);
 //Asimila mayusculas
 			if (numAscii >= 65 && numAscii <= 90){
 				numAscii = String.fromCharCode((numAscii - 65 + parseInt(key)) % 26 + 65);
 				result += numAscii;
-				console.log("result" + result);
-				document.getElementById("message").innerHTML = result;
+				console.log(result);
 //Asimila minisculas
 			} else if (numAscii >= 97 && numAscii <= 122){
 				numAscii = String.fromCharCode((numAscii - 97 + parseInt(key)) % 26 + 97);
 				result += numAscii;
-				let userMessage = document.getElementById('message').innerHTML;
-				let replace = resultMessage.replace("message", "result" 	);
-				console.log("result" + result);
-
+				let userMessage = document.getElementById('message').value;
+				let replace = userMessage.replace( userMessage, result);
+				document.getElementById("message").value = replace;
+				console.log(result);
 //Asimila el espacio " "
-			} else if (numAscii = 32){
-				numAscii += " ";
+} else if (numAscii === 32){
+				result += " ";
 			}
 		}
 	}
